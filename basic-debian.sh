df77ec281101f81af1eb8958ccf7328e344da463
DariusDacianMaterialInside <54778569+DariusDacianMaterialInside@users.noreply.github.com>
2020-12-23 16:05:50 +0200

    Add files via upload

---
+++ basic-debian.sh
@@ -0,0 +1,1 @@
+apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free firmware-linux-nonfree firmware-linux i3-wm i3blocks i3lock brightnessctl pulseaudio alsa-utils htop kitty xfe chromium audacious feh gimp kdenlive guvciview simplescreenrecorder
