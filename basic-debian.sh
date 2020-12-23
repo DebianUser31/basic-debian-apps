aa14b13ec09aafa88a201b9263ffa76bbcd19e83
DariusDacianMaterialInside <54778569+DariusDacianMaterialInside@users.noreply.github.com>
2020-12-23 16:37:37 +0200

    Update basic-debian.sh

--- basic-debian.sh
+++ basic-debian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free firmware-linux-nonfree firmware-linux i3-wm i3blocks i3lock brightnessctl pulseaudio alsa-utils htop kitty xfe chromium audacious feh gimp kdenlive guvciview simplescreenrecorder
+apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free firmware-linux-nonfree firmware-linux i3-wm i3blocks i3lock brightnessctl pulseaudio alsa-utils htop kitty xfe chromium audacious feh gimp kdenlive guvciview simplescreenrecorder mplayer
