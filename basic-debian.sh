8c8871c32a34451ae3dfa5179d7f012062a6f108
DebianUser <54778569+DebianUser31@users.noreply.github.com>
2020-12-26 17:41:17 +0200

    Update basic-debian.sh

--- basic-debian.sh
+++ basic-debian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free i3-wm i3blocks i3lock suckless-tools brightnessctl pulseaudio pulseaudio-utils pavucontrol alsa-utils htop xfe firefox-esr audacious feh gimp kdenlive guvcview simplescreenrecorder mplayer zathura zathura-ps fonts-font-awesome && systemctl --user enable pulseaudio
+sudo apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free i3-wm i3blocks i3lock suckless-tools brightnessctl pulseaudio pulseaudio-utils pavucontrol alsa-utils htop gnome-terminal xfe firefox-esr audacious feh gimp kdenlive guvcview simplescreenrecorder mplayer zathura zathura-ps fonts-font-awesome && systemctl --user enable pulseaudio
