28b7384830f1d5527ced192b0e1d6a773231d9e7
DebianUser <54778569+DebianUser31@users.noreply.github.com>
2020-12-26 20:20:47 +0200

    Update basic-debian.sh

--- basic-debian.sh
+++ basic-debian.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free i3-wm i3blocks i3lock suckless-tools brightnessctl pulseaudio pulseaudio-utils pavucontrol alsa-utils htop gnome-terminal xfe firefox-esr audacious feh gimp kdenlive guvcview simplescreenrecorder mplayer zathura zathura-ps fonts-font-awesome && systemctl --user enable pulseaudio
+#installing basic "basic" apps and enabling pulseaduio
+sudo apt install xorg xutils xserver-xorg xinit xserver-xorg-video-intel firmware-linux-free i3-wm i3blocks i3lock suckless-tools brightnessctl pulseaudio pulseaudio-utils pavucontrol alsa-utils htop gnome-terminal xfe firefox-esr audacious feh gimp kdenlive guvcview simplescreenrecorder mplayer zathura zathura-ps fonts-font-awesome 
+systemctl --user enable pulseaudio
+
+#For youtube-dl you need the latest package for it to actaully work
+#you get it by typing this in the terminal
+#sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+#sudo chmod a+rx /usr/local/bin/youtube-dl
+#
+#to update youtube-dl you run this command
+#youtube-dl -U
